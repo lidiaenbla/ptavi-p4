68e1b74a94993652315de82500d0641acc3a4fe2
lidiaenbla <lidiaeb1993@gmail.com>
2016-10-23 09:47:12 +0200

    haciendo ej6

--- server.py
+++ server.py
@@ -6,21 +6,27 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 
+clientes = {}
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+def registro(ip):
+    clientes["dir"] = ip 
+
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     def handle(self):
-        print(
-            "IP:" + str(self.client_address[0]),
-            "\nPort:" + str(self.client_address[1]))
-        self.wfile.write(b"Hemos recibido tu peticion")
+        clientes = {}
+        IP = str(self.client_address[0])
+        Port = str(self.client_address[1])
+        registro(IP)
+        print("IP:" + IP + "\nPort:" + Port)
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', 6001), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()

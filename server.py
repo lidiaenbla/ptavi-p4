f35782389d973c97c5eeb6ae2b34392849334590
lidiaenbla <lidiaeb1993@gmail.com>
2016-10-24 16:59:11 +0200

    implementado expires

--- server.py
+++ server.py
@@ -5,34 +5,47 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import time
 
 dicc_cliente = {}
 clientes = []
-
-
-def Register(ip, sip):
-
-    dicc_cliente[sip] = ip
-    clientes.append(dicc_cliente)
-
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    dicc_cliente = {}
+    def comprobarExpires(self):
+        deleteList = []
+        horaActual = time.gmtime(time.time())
+        for cliente in self.dicc_cliente:
+            if time.strptime(self.dicc_cliente[cliente][1], '%Y-%m-%d %H:%M:%S') <= horaActual:
+                deleteList.append(cliente)
+                print("cliente borrado", cliente)
+        for i in deleteList:
+            del self.dicc_cliente[i]
+
+    def Register(self, ip, sip, expires):
+        """
+        Registrar a clientes en el diccionario
+        """
+        self.comprobarExpires()
+        self.dicc_cliente[sip[4:]] = [ip, expires]
+        print(self.dicc_cliente)
+
     def handle(self):
         sip = []
         IP = str(self.client_address[0])
         Port = str(self.client_address[1])
         print("IP:" + IP + "\nPort:" + Port)
         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-        for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-            linea = line.decode('utf-8')
-            for i in linea.split(" "):
-                if "@" in i:
-                    sip = i
-        Register(IP, sip)
+        line = self.rfile.read()
+        print("El cliente nos manda ", line.decode('utf-8'))
+        linea = line.decode('utf-8').split()
+        sip = linea[1]
+        expires = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() + float(linea[4])))
+        self.Register(IP, sip, expires)
+
 
 if __name__ == "__main__":
     serv = socketserver.UDPServer(('', 6001), SIPRegisterHandler)
